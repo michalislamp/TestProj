6c5f866c9a50ec3b6e70d192dcf3abb470766b0c
michalis <michalis@DESKTOP-N8IEITI>
2021-04-11 22:55:43 +0300

    Second

---
+++ TEST TEST/src/TEST/testFunc.java
@@ -0,0 +1,9 @@
+package TEST;
+
+public class testFunc {
+
+	
+	public testFunc() {
+		System.out.println("Test func");
+	}
+}
--- TEST TEST/src/TEST/testGitHub.java
+++ TEST TEST/src/TEST/testGitHub.java
@@ -2,7 +2,9 @@ package TEST;
 
 public class testGitHub {
 
+	
 	public static void main(String[] args) {
 		System.out.println("Hello Wordl!");
+		testFunc test = new testFunc();
 	}
 }
