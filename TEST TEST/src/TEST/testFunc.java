bdc2b4aae71974907b0d0e214bb83829d89aa759
michalislamp <82032228+michalislamp@users.noreply.github.com>
2021-04-17 17:00:07 +0300

    Update testFunc.java

--- TEST TEST/src/TEST/testFunc.java
+++ TEST TEST/src/TEST/testFunc.java
@@ -6,5 +6,6 @@ public class testFunc {
 	public testFunc() {
 		System.out.println("Test func!");
 		System.out.println("O pantelis einai trelos");
+		System.out.println("O pantelis den einai einai trelos");
 	}
 }
